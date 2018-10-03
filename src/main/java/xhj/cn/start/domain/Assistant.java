244f32e2e00446b5c932d114f7818ffc7c79c33c
clx <851591666@qq.com>
2018-10-03 16:52:01 +0800

    change

--- src/main/java/xhj/cn/start/domain/Assistant.java
+++ src/main/java/xhj/cn/start/domain/Assistant.java
@@ -7,16 +7,28 @@ import cn.topoints.utils.data.rds.RDSAnnID;
 @RDSAnnEntity(alias = "tb_assistant_message")
 public class Assistant {
 	
+	/**
+	 * 营业员ID
+	 */
 	@RDSAnnID
 	@RDSAnnField(column = RDSAnnField.ID)
 	public Long assistantId;
 	
+	/**
+	 * 营业员名称
+	 */
 	@RDSAnnField(column = "VARCHAR(16)")
 	public String assistantName;
 	
+	/**
+	 * 营业员所属门店对应poi_id
+	 */
 	@RDSAnnField(column = "VARCHAR(64)")
 	public String assistantStorePoiId;
 	
+	/**
+	 * 获取二维码对应参数
+	 */
 	@RDSAnnField(column = "VARCHAR(64)")
 	public String ticket;
 
